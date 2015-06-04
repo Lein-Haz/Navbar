5dddf630e826c4a3ea9c945e7ea960310112cfa8
Lein-Haz <paz5x4@mail.umsl.edu>
2015-06-04 00:34:38 -0500

    I think I said to remove any script tags from loaded pages

--- js/contentLoader.js
+++ js/contentLoader.js
@@ -3,27 +3,52 @@
  */
 $(document).ready(function(){
 
-    $("#content").load("home.html")
-
+    $("#content").load("home.html");
 
     $("#home").click(function() {
-        $("#content").load("home.html")
+        $("#content").load("home.html");
     });
     $("#services").click(function() {
-        $("#content").load("services.html")
+        $("#content").load("services.html");
     });
     $("#products").click(function() {
-        $("#content").load("products.html")
+        $("#content").load("products.html");
     });
     $("#partners").click(function() {
-        $("#content").load("partners.html")
+        $("#content").load("partners.html");
     });
     $("#about").click(function() {
-        $("#content").load("about.html")
+        $("#content").load("about.html");
     });
     $("#locations").click(function() {
-        $("#content").load("maps.html")
+        $("#content").load("maps.html");
+
+        loadScript();
+
+        /*otherLoad();
+        google.maps.event.addDomListener(this, 'load', initialize());*/
     });
+    function loadScript() {
+        var script;
+        var len = $('script[src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&callback=initialize"]').length;
+        if(len < 1)
+        {
+            script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&callback=initialize';
+            document.body.appendChild(script);
+        } else{
+            //cause api hate
+            script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&callback=initialize';
+            document.body.appendChild(script);
+
+            //initialize();//nope
+
+            //google.maps.event.addDomListener($('#map-canvas'), "load", this.initialize());//nope
+        }
+    }
 });
 
 
--- js/maps.js
+++ js/maps.js
@@ -1,19 +1,8 @@
 /**
  * Created by Nickkaczmarek on 6/2/15.
  */
-    console.log("intitializing");
-    var map;
-    function initialize() {
-        alert("initialize");
-        var mapOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(-34.397, 150.644)
-        };
-        map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-    }
 
-    google.maps.event.addDomListener($("#map-canvas"), 'load', initialize);
+
 
         //var bounds = new google.maps.LatLngBounds();
         //
@@ -25,66 +14,32 @@
         //
         //];
 
-
-        var mapOptions={
-            center: new google.maps.LatLng(39.653657, -91.734908),
-            zoom:7,
-            mapTypeId:google.maps.MapTypeId.ROADMAP,
-            zoomControl:true,
-            zoomControlOptions: {
-                style:google.maps.ZoomControlStyle.DEFAULT
-            }
-        };
-
-
-
-
-        //markers
-
-    //    var infowindow = new google.maps.InfoWindow();
-    //
-    //    var marker, i;
-    //
-    //    for (i = 0; i < locations.length; i++) {
-    //        marker = new google.maps.Marker({
-    //            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-    //            map: this.map,
-    //            icon: 'images/marker.png'
-    //        });
-    //
-    //        bounds.extend(marker.position);
-    //
-    //        google.maps.event.addListener(marker, 'click', (function (marker, i) {
-    //            return function () {
-    //                infowindow.setContent(locations[i][0]);
-    //                infowindow.open(map, marker);
-    //            }
-    //        })(marker, i));
-    //
-    //        google.maps.event.addListener(marker,'click',function() {
-    //            map.setZoom(12);
-    //            map.setCenter(this.getPosition());
-    //        });
-    //
-    //        //google.maps.event.addListener(map,'center_changed',function() {
-    //        //    map.setZoom(9);
-    //        //    window.setTimeout(function() {
-    //        //        map.panTo(this.getPosition());
-    //        //    },3000);
-    //        //});
-    //    }
-    //
-    //    map.fitBounds(bounds);
-    //
-    //    var listener = google.maps.event.addListener(map, "idle", function () {
-    //        map.setZoom(8);
-    //        google.maps.event.removeListener(listener);
-    //    });
-    //
-    //google.maps.event.addDomListener(window, 'load', initialize);
+function initialize() {
+    var map;
+    console.log("intitializing");
+    var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(39.653657, -91.734908),
+        mapTypeId:google.maps.MapTypeId.ROADMAP,
+        zoomControl:true,
+        zoomControlOptions: {
+            style:google.maps.ZoomControlStyle.DEFAULT
+        }
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+}
+/*var mapOptions={
+    center: new google.maps.LatLng(39.653657, -91.734908),
+    zoom:7,
+    mapTypeId:google.maps.MapTypeId.ROADMAP,
+    zoomControl:true,
+    zoomControlOptions: {
+        style:google.maps.ZoomControlStyle.DEFAULT
+    }
+};*/
 
 
-    alert("Farts");
+
 
 
 
